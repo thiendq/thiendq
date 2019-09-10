ca45e93302dde02b7490d983d379e474ed9616f6
thiendq <thiendq.pcbd@gmail.com>
2019-09-10 09:26:38 +0700

    1 file modified, 2 added

---
+++ src/app/usercontrol/xoathu/laydston/laydston.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-laydston',
+  templateUrl: './laydston.component.html',
+  styleUrls: ['./laydston.component.css']
+})
+export class LaydstonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
