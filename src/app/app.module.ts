b07c9be4d211514e3c4be52377e2be6bb0a47ac2
thiendq <thiendq.pcbd@gmail.com>
2019-09-10 13:06:57 +0700

    2 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
+//import { NgModule } from '@angular/core';
+//import { BrowserModule } from '@angular/platform-browser';
+//import { FormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DemoMaterialModule } from './material-module';
+import { enableProdMode } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { LaydstonComponent } from './usercontrol/xoathu/laydston/laydston.component';
-import {Routing} from './routing';
+import { Routing } from './routing';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, Routing ],
-  declarations: [ AppComponent, HelloComponent, LaydstonComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, Routing,DemoMaterialModule],
+  declarations: [AppComponent, HelloComponent, LaydstonComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,7 @@
 import './polyfills';
-
-import {HttpClientModule} from '@angular/common/http'
-import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatNativeDateModule} from '@angular/material/core';
-import {BrowserModule} from '@angular/platform-browser';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {DemoMaterialModule} from './material-module';
-import { enableProdMode } from '@angular/core';
+
+
 
 import { AppModule } from './app/app.module';
 //import {LaydstonComponent} from './app/usercontrol/xoathu/laydston/laydston.component'
