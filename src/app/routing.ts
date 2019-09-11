f35e0341df2bcb7426d8bfb1a560f59e4fbda8de
thiendq <thiendq.pcbd@gmail.com>
2019-09-11 11:27:45 +0700

    1 file modified

--- src/app/routing.ts
+++ src/app/routing.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
+import { Type, ModuleWithProviders, NgModule } from '@angular/core';
 
 import { LaydstonComponent } from './usercontrol/xoathu/laydston/laydston.component';
+import { DemoMaterialModule } from './material-module';
 import { AppComponent } from './app.component'
+
+const components: any[] | Type<any> | ModuleWithProviders<{}> = [
+LaydstonComponent,DemoMaterialModule
+];
 
 const routes: Routes = [
   { path: '', redirectTo: 'getting-started', pathMatch: 'full' },
