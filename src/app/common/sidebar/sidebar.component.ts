57ea6f0c47bfcda65294f36dd55cbd9a4d3afc22
thiendq <thiendq.pcbd@gmail.com>
2019-09-12 11:28:16 +0700

    2 files modified, 1 added

--- src/app/common/sidebar/sidebar.component.ts
+++ src/app/common/sidebar/sidebar.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import * as data from './../samples.json';
 
 @Component({
-  selector: 'app-sidebar',
+  selector: 'ej-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
 
-  constructor() { }
+export class SideBarComponent {
+  reportViewer = 'report-viewer/';
+  reportDesigner = 'report-designer/';
+  reportViewerSample = data.ReportViewer.samples;
+  reportDesignerSample = data.ReportDesigner.samples;
+  selectedPath: string = undefined;
 
-  ngOnInit() {
+  constructor(private router: Router) { }
+
+  public onSampleClick(config): void {
+    this.router.navigate([config.prependHash + config.sample.routerPath]);
+  }
+
+  public tocSelection(toc: HTMLElement): string {
+    if (!toc.classList.contains('ej-doc-toc-selected')) {
+      toc.focus();
+    }
+    return 'ej-doc-toc-selected';
   }
 
 }
